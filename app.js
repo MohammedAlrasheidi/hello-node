20a138ac9c77c73aab5f9ff8913a59cc028af09c
Mohammed <156168748+MohammedAlrasheidi@users.noreply.github.com>
2024-08-28 20:56:58 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
 
-console.log('im on a node server, yo');
+console.log('I'm on a Node server, yo');
+
+// Serve static files from the current directory
+app.use(express.static('./'));
 
 app.get('/', function (req, res) {
-  res.send('Hello Node from Ex on local dev box')
-})
+  res.send('Hello Node from Ex on local dev box');
+});
 
-app.listen(5000)
+app.listen(5000, () => {
+  console.log('Server is running on http://localhost:5000');
+});
+
