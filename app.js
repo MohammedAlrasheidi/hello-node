b5308e774c03ba00facbb6ba8e481998d0fe203e
Mohammed <156168748+MohammedAlrasheidi@users.noreply.github.com>
2024-09-17 17:32:39 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,14 +1,122 @@
-const express = require('express');
-const path = require('path');
-const app = express();
+require('dotenv').config()
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+const { urlencoded } = require('body-parser')
+const { ObjectId } = require('mongodb')
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://barry:${process.env.MONGO_PWD}@cluster0.5abxx.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`; 
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.urlencoded({ extended: true }))
+app.set('view engine', 'ejs')
+app.use(express.static('./public/'))
 
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'index.html'));
+console.log(uri);
+
+console.log('im on a node server change that and that tanad f, yo');
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
 });
 
-app.listen(3000, () => {
-  console.log('Server is running on http://localhost:3000');
-});
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
 
+// function whateverNameOfIt (params) {}
+// ()=>{}
+
+app.get('/', function (req, res) {
+  // res.send('Hello Node from Ex on local dev box')
+  res.sendFile('index.html');
+})
+
+app.get('/ejs', (req,res)=>{
+``
+  res.render('index', {
+    myServerVariable : "something from server"
+  });
+
+  //can you get content from client...to console? 
+})
+
+app.get('/read', async (req,res)=>{
+
+  console.log('in /mongo');
+  await client.connect();
+  
+  console.log('connected?');
+  // Send a ping to confirm a successful connection
+  
+  let result = await client.db("barrys-db").collection("whatever-collection")
+    .find({}).toArray(); 
+  console.log(result); 
+
+  res.render('mongo', {
+    postData : result
+  });
+
+})
+
+app.get('/insert', async (req,res)=> {
+
+  console.log('in /insert');
+  //connect to db,
+  await client.connect();
+  //point to the collection 
+  await client.db("barrys-db").collection("whatever-collection").insertOne({ post: 'hardcoded post insert '});
+  await client.db("barrys-db").collection("whatever-collection").insertOne({ iJustMadeThisUp: 'hardcoded new key '});  
+  //insert into it
+  res.render('insert');
+
+}); 
+
+app.post('/update/:id', async (req,res)=>{
+
+  console.log("req.parms.id: ", req.params.id)
+
+  client.connect; 
+  const collection = client.db("barrys-db").collection("whatever-collection");
+  let result = await collection.findOneAndUpdate( 
+  {"_id": new ObjectId(req.params.id)}, { $set: {"post": "NEW POST" } }
+)
+.then(result => {
+  console.log(result); 
+  res.redirect('/read');
+})
+}); 
+
+app.post('/delete/:id', async (req,res)=>{
+
+  console.log("req.parms.id: ", req.params.id)
+
+  client.connect; 
+  const collection = client.db("barrys-db").collection("whatever-collection");
+  let result = await collection.findOneAndDelete( 
+  {"_id": new ObjectId(req.params.id)})
+
+.then(result => {
+  console.log(result); 
+  res.redirect('/read');
+})
+
+  //insert into it
+
+})
+
+app.listen(5500);
