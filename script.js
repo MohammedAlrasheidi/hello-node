3769dd3ece57e4b5e3bdd63b8eb7e29c8dbe860d
Mohammed <malrasheidi@una.edu>
2024-10-05 15:33:50 -0500

    Update script.js

--- script.js
+++ script.js
@@ -35,7 +35,7 @@ document.addEventListener('DOMContentLoaded', function() {
         const natoAlphabet = {
             'A': 'Alpha', 'B': 'Bravo', 'C': 'Charlie', 'D': 'Delta', 'E': 'Echo',
             'F': 'Foxtrot', 'G': 'Golf', 'H': 'Hotel', 'I': 'India', 'J': 'Juliett',
-            'K': 'Kilo', 'L': 'Lima', 'M': 'Mike', 'N': 'November', 'O': 'Ohh Wow',
+            'K': 'Kilo', 'L': 'Lima', 'M': 'Mike', 'N': 'November', 'O': 'Oscar',
             'P': 'Papa', 'Q': 'Quebec', 'R': 'Romeo', 'S': 'Sierra', 'T': 'Tango',
             'U': 'Uniform', 'V': 'Victor', 'W': 'Whiskey', 'X': 'X-ray', 'Y': 'Yankee',
             'Z': 'Zulu'
